1e47bff298117c4fbec34dad6525ab274e0a028b
Diego Caro <diegocaro@gmail.com>
2016-10-06 21:02:06 -0300

    Adding 2d normal distribution

--- tareas/tarea1/random2d.py
+++ tareas/tarea1/random2d.py
@@ -1,6 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-points = np.random.rand(30, 2)   # 30 random points in 2-D
-plt.plot(points[:,0], points[:,1], 'o')
-plt.show()
+def plot_rand():
+    points_rand = np.random.rand(100, 2)   # 30 random points in 2-D
+    x_rand = points_rand[:,0]
+    y_rand = points_rand[:,1]
+    plt.plot(x_rand, y_rand, 'o')
+    plt.show()
+    
+def plot_normal():
+    mean = [1, 2]
+    cov = [[1, 0], [0, 1]]  # diagonal covariance
+    x_normal, y_normal = np.random.multivariate_normal(mean, cov, 100).T
+    plt.plot(x_normal, y_normal, 'x')
+    plt.show()
+
+plot_rand()
+plot_normal()
+
