151e4433d592a71e1a182502ab2b6b6875eb204d
Diego Caro <diegocaro@gmail.com>
2016-10-18 17:02:52 -0300

    Habia un error en el script de la solucion anterior

--- solemnes/s1-tarjetas.py
+++ solemnes/s1-tarjetas.py
@@ -20,52 +20,78 @@ def tarjetas(A):
   mid = len(A)//2
   B1 = tarjetas(A[:mid])
   B2 = tarjetas(A[mid:])
-  
+
   # Combinar soluciones
   C = []
   if len(B1) == 1:
+    C.append(B1[0])
     for b in B2:
-      if equiv(B1[0], b) == False:
+      if equiv(B1[0], b):
         C.append(b)
   elif len(B2) == 1:
+    C.append(B2[0])
     for b in B1:
-      if equiv(B2[0], b) == False:
+      if equiv(B2[0], b):
         C.append(b)
+  if len(C) > len(B1+B2)/2: 
+    return [C[0]]
   else:
-    C.extend(B1)
-    C.extend(B2)
-  return C
+    return A
 
-def answer(A, pr=False):
+def answer(A):
+  global calls
+  calls = 0
   ans = tarjetas(A)
-  if pr: print(ans)
-  return  len(ans) <= len(A)//2
+  if len(A) <= 10: 
+    print(A)
+    print(ans)
+  return  len(ans) == 1
 
 if __name__=='__main__':
-  t = list("abababbb")
-  print(answer(t,True))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abababbb"*1000)
+  t = list("bbbbbbbb")
   print(answer(t))
   print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
   
+  t = list("abbbdbbb")
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("abababbb")
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("bbbdabcb")
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("bbdabbcb")
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
   
   t = list("abababab")
-  print(answer(t,True))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abababab"*1000)
   print(answer(t))
   print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
   
-  
-  calls = 0
   t = list("abcdefgh")
-  print(answer(t, True))
+  print(answer(t))
   print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
   
-  calls = 0
-  t = list("abcdefgh"*1000)
+  t = list("xyzbbbbc")
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("aaffccdd")
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("abababbb"*1)
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("abababab"*1)
+  print(answer(t))
+  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  
+  t = list("abcdefgh"*1)
   print(answer(t))
   print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
