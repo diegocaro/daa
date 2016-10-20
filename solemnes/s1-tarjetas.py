df08218152936ebd682857b71f40390873a03dc7
Diego Caro <diegocaro@gmail.com>
2016-10-20 15:17:40 -0300

    solucionado un problema al chequar si la tarjeta es mayoritaria

--- solemnes/s1-tarjetas.py
+++ solemnes/s1-tarjetas.py
@@ -1,4 +1,5 @@
 from math import log
+import unittest
 
 # for counting the number of calls to equiv()
 calls = 0 
@@ -20,78 +21,42 @@ def tarjetas(A):
   mid = len(A)//2
   B1 = tarjetas(A[:mid])
   B2 = tarjetas(A[mid:])
-
-  # Combinar soluciones
-  C = []
   if len(B1) == 1:
-    C.append(B1[0])
-    for b in B2:
-      if equiv(B1[0], b):
-        C.append(b)
-  elif len(B2) == 1:
-    C.append(B2[0])
-    for b in B1:
-      if equiv(B2[0], b):
-        C.append(b)
-  if len(C) > len(B1+B2)/2: 
-    return [C[0]]
-  else:
-    return A
+    c = 0
+    for t in A:
+      if equiv(B1[0], t): c += 1
+    if c > len(A)/2:
+      return B1
+  if len(B2) == 1:
+    c = 0
+    for t in A:
+      if equiv(B2[0], t): c += 1
+    if c > len(A)/2:
+      return B2
+  return A
+  
 
 def answer(A):
   global calls
   calls = 0
   ans = tarjetas(A)
-  if len(A) <= 10: 
-    print(A)
-    print(ans)
-  return  len(ans) == 1
+  return len(ans) < len(A)/2
 
+class TestInsertionSort(unittest.TestCase):
+  def testTrue(self):
+    cases = ["abababbb", "abaabbbb", "bbbbbbbb", "abbbdbbb", "abababbb", "bbdabbcb"]
+    for case in cases:
+      t = list(case)
+      if False == answer(t):
+        print("input: ", t)
+        self.fail( "algorithm failed" )
+        
+  def testFalse(self):
+    cases = ["abcdefgh", "abababab", "xyzbbbbc", "aaffccdd"]
+    for case in cases:
+      t = list(case)
+      if True == answer(t):
+        self.fail( "algorithm failed" )
+  
 if __name__=='__main__':
-  t = list("bbbbbbbb")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abbbdbbb")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abababbb")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("bbbdabcb")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("bbdabbcb")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abababab")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abcdefgh")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("xyzbbbbc")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("aaffccdd")
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abababbb"*1)
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abababab"*1)
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
-  
-  t = list("abcdefgh"*1)
-  print(answer(t))
-  print("calls:", calls, " theory worst case:", int(len(t) * log(len(t),2)))
+  unittest.main()
