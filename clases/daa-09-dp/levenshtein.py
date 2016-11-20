96d61f4068f978fb8169b1e33c556191d21d6e8c
Rocio <rocio.venegas@mail.udp.cl>
2016-11-20 17:35:54 -0300

    codigo corregido

--- clases/daa-09-dp/levenshtein.py
+++ clases/daa-09-dp/levenshtein.py
@@ -1,8 +1,7 @@
-import numpy
 def levenshtein(s1, s2):
     p1 = len(s1)
     p2 = len(s2)
-    matriz = numpy.zeros((p1, p2))
+    matriz = [[0 for _ in range(p2)] for __ in range(p1)]
     for i in range(p1):
         matriz[i][0] = i
     for j in range (p2):
@@ -21,7 +20,7 @@ def levenshtein(s1, s2):
 def Space_Efficient_Alignmen(s1, s2):
     p1 = len(s1)
     p2 = len(s2)
-    matriz = numpy.zeros((p1, 2))
+    matriz = [[0 for _ in range(p2)] for __ in range(p1)]
     for i in range(p1):
         matriz[i][0] = i
     for j in range(1,p2):
@@ -35,13 +34,37 @@ def Space_Efficient_Alignmen(s1, s2):
 
         for i in range(p1):
             matriz[i][0] = matriz[i][1]
-
     return matriz[p1-1][1]
 
 # usara por defecto la funcion Space_Efficient_Alignmen
 def edit_distance(s1, s2, memory_space_efficent = True):
+    # Caso borde cuando hay al menos un string vacio
+    if (len(s2) == 0):
+        return len(s1)
+    elif (len(s2) == 0):
+        return len(s1)
     s1 = " " + s1
     s2 = " " + s2
     if (memory_space_efficent):
         return Space_Efficient_Alignmen(s1, s2)
     return levenshtein(s1, s2)
+
+def test():
+    palabras = [("casa", "cala"), ("levenshtein", "meilenstein"), ("facil","dificil"),
+                ("sonny","snowy"), ("",""), ("a", "a"), ("a", "")]
+    resultados = [1,4,3,3,0,0,1]
+    for i in range (len(palabras)):
+        a = edit_distance(palabras[i][0], palabras[i][1])
+        b = edit_distance(palabras[i][0], palabras[i][1], False)
+        if ( a != resultados[i]):
+            print("error, resultado esperado: {} \n resultado obtenido: {} ".format(resultados[i], a) )
+            exit (1)
+        elif ( b != resultados[i]):
+            print("error, resultado esperado: {} \n resultado obtenido: {} ".format(resultados[i], b) )
+            exit (1)
+        else:
+            print("Test {} pasado.".format(i+1))
+    exit (0)
+
+if __name__ == '__main__':
+    test()
